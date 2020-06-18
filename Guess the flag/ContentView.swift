08aeaba155ccc6d908908f182e0f99b52dc0e8a3
Vadym Hordov <vadymhordov@gmail.com>
2020-06-18 19:28:52 -0400

    Initialized xCode Project

---
+++ Guess the flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess the flag
+//
+//  Created by Vadym Hordov on 2020-06-18.
+//  Copyright © 2020 Vadym Hordov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
