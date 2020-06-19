3ba3b14d45d603ab9db5b0bbbedbbf2ff60342ef
Dmitry Grechko <dmitry.grechko@deskree.com>
2020-06-18 20:12:10 -0400

    Some examples

--- Guess the flag/ContentView.swift
+++ Guess the flag/ContentView.swift
@@ -9,8 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert: Bool = false
+    @State private var name: String = "Vadym"
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack() {
+            Color.green.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                TextField("Name", text: $name)
+                HStack(spacing: 20) {
+                    Button("Show alert") {
+                        self.showingAlert = true
+                    }
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("This is alert!"), message: Text("We are showing alert to \(name)!"))
+                    }
+                    Button(action: {
+                        print("Tapped!")
+                    }) {
+                        VStack(spacing: 10) {
+                            Text("Tap me!")
+                            Image(systemName: "heart")
+                                .renderingMode(.original)
+                        }
+                    }
+                    Button(action: {
+                        print("editing!")
+                    }) {
+                        Text("Edit")
+                        Image(systemName: "pencil")
+                    }
+                }
+                Spacer()
+                HStack(spacing: 20) {
+                    Text("First")
+                    Text("Second")
+                    Text("Third")
+                }
+                Spacer()
+            }
+        }
     }
 }
 
